90036ffde86be189b7db272e10c9644ec705456b
Diego Florez <andresflorezrincon11@gmail.com>
2025-09-23 11:14:09 -0500

    #50 Mejorando nuestra Arquitectura Parte 2 | Sistema de Ventas

--- POS.Infraestructure/Persistences/Context/Configuration/DocumentTypeConfiguration.cs
+++ POS.Infraestructure/Persistences/Context/Configuration/DocumentTypeConfiguration.cs
@@ -13,6 +13,11 @@ namespace POS.Infraestructure.Persistences.Context.Configuration
     {
         public void Configure(EntityTypeBuilder<DocumentType> builder)
         {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id)
+                .HasColumnName("DocumentTypeId");
+
             builder.Property(e => e.Abbreviation)
                   .HasMaxLength(5)
                   .IsUnicode(false);
